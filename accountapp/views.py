d045f56f913d2d033748eeee91f6414d0fbf7a99
Changha Hwang <ssw990531@naver.com>
2021-02-08 20:49:25 +0900

    base fix

--- accountapp/views.py
+++ accountapp/views.py
@@ -3,12 +3,16 @@ from django.contrib.auth.models import User
 from django.contrib import auth
 
 def login(request) :
+
     if request.method == "POST":
+
         useremail = request.POST.get('useremail', None)
         password = request.POST.get('password', None)
-        user = auth.authenticate(useremail=useremail, password=password)
+        user = auth.authenticate(username=useremail, password=password)
+
         if user is not None:
             auth.login(request, user)
+
             return redirect("/echozapp/index/")
         else:
             return render(request, 'login.html', {'error': '사용자 아이디 또는 패스워드가 틀립니다.'})
@@ -19,21 +23,22 @@ def register(request):
     res_data = None
     if request.method =='POST':
         useremail = request.POST.get('useremail', None)
-        username = request.POST.get('username', None)
+        firstname = request.POST.get('firstname', None)
+        lastname = request.POST.get('lastname', None)
         password = request.POST.get('password', None)
         repassword = request.POST.get('repassword',None)
         res_data = {}
-        if User.objects.filter(useremail=useremail):
+        if User.objects.filter(username=useremail):
             res_data['error']='이미 가입된 아이디(이메일주소)입니다.'
         elif password != repassword:
             res_data['error']='비밀번호가 다릅니다.'
         else:
-            user = User.objects.create_user(useremail = useremail,
-                            username = username,
-                            password = password)
+            user = User.objects.create_user(username = useremail,
+                                    first_name=firstname,
+                                    last_name=lastname,
+                                    password = password)
             auth.login(request, user)
-            redirect("/echozapp/index/")
+            return render(request,"index.html")
     return render(request, 'register.html', res_data)
-
 
 # Create your views here.
