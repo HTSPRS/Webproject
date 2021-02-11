4770034201ae1e9e0d0b38ed7b7890ecd9a8ad98
Changha Hwang <ssw990531@naver.com>
2021-02-11 14:11:36 +0900

    Board&login

--- echozapp/models.py
+++ echozapp/models.py
@@ -1,8 +1,9 @@
 from django.db import models
 from django.contrib.auth.models import User
 
-class Post(models.Model):
 
+class Post(models.Model):
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
     title = models.CharField(max_length=30)
     content = models.TextField()
     writedate = models.DateTimeField(auto_now_add=True)
